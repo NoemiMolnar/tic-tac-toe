69651a9158e9db011aeb7c1c08ab5f379c4b184b
NoemiMolnar <noemi.m.molnar@gmail.com>
2018-08-11 17:42:02 +0200

    refactoring

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -9,21 +9,13 @@ export default class MainController {
     return games
   }
 
+  colorArray: string[] = ['red', 'blue', 'green', 'yellow', 'magenta']
+
   newColor = () => {
     const random = Math.floor(Math.random() * 5)
-    switch (random) {
-      case 0:
-        return "red"
-      case 1:
-        return "blue"
-      case 2:
-        return "green"
-      case 3:
-        return "yellow"
-      default:
-        return "magenta"
-    }
+    return this.colorArray[random]
   }
+
 
   @Post('/games')
   @HttpCode(201)
@@ -35,18 +27,11 @@ export default class MainController {
     return game.save()
   }
 
-  // @Get('/games/:id')
-  // async getGame(
-  //   @Param('id') id: number
-  // ): Promise<Game | undefined> {
-  //   return await Game.findOne(id)
-  // }
-
-  moves = (board1, board2) => 
-  board1
-    .map((row, y) => row.filter((cell, x) => board2[y][x] !== cell))
-    .reduce((a, b) => a.concat(b))
-    .length
+  moves = (board1, board2) =>
+    board1
+      .map((row, y) => row.filter((cell, x) => board2[y][x] !== cell))
+      .reduce((a, b) => a.concat(b))
+      .length
 
 
   @Put('/games/:id')
@@ -60,34 +45,22 @@ export default class MainController {
     if (!game) {
       throw new NotFoundError('Cannot find page')
     } else {
-      if (Object.keys(update).includes('id')) {
+      if (update.id !== undefined) {
         console.log("changing the id is not allowed")
         delete update.id
       }
-      if(Object.keys(update).includes('board')){
-        if(this.moves(game.board, update.board)>1){
-          return undefined
-        }      
+      if (update.color !== undefined && !this.colorArray.includes(update.color)) {
+        return undefined
       }
-    
-      let newColor = this.newColor()
-      while (newColor===game.color){
-        newColor = this.newColor()
-      }
-      update.color = newColor 
-
-      return Game.merge(game, update).save()
     }
+    if (update.board !== undefined && this.moves(game.board, update.board) > 1) {
+      return undefined
+
+    }
+    return Game.merge(game, update).save()
   }
-
-
-  // @Put("/games/:id")
-  // put(@Param("id") id: number, 
-  //   @Body() game: Game)
-  // {
-  //   return Game.findOne(id);
-  // }
-
 }
+
+
 
 
